5fa575fac814cd180c8da0a2095f0ed21d997816
Christian Avemark <christian@avemark.se>
2014-03-11 16:51:59 +0100

    Styyyle!

--- lib/server.rb
+++ lib/server.rb
@@ -1,22 +1,20 @@
 class Server < MiniSmtpServer
-  
   def initialize(*args)
-    @lists = ListStorage.new
+    @storage = ListStorage.new
+    @queue = MailQueue.new
     @logger = Logger.new("./logs/received_mail.log")
     super
   end
 
-  def set_storage(list_storage)
-    @lists = list_storage
-  end
+  attr_writer :storage, :queue
 
   def new_message_event(message_hash)
     @logger.info "Message to:#{message_hash[:to]} received"
-    @logger.info "sent to: #{@lists.get(message_hash[:to]).join(' ')}"
+    @logger.info "sent to: #{@storage.get(message_hash[:to]).join(' ')}"
     puts "# New Email received"
     puts "-- to: #{message_hash[:to]}"
     puts "-- recipients:"
-    if recipients = @lists.get(message_hash[:to])
+    if recipients = @storage.get(message_hash[:to])
       recipients.each do |adress|
         puts "--> #{adress}"
       end
