9d192cca9baddefbedcefb73cc0f2194c3aad214
root <root@fossil-fuel.ben-and-jerrys.stacken.kth.se>
2014-03-11 16:19:53 +0100

    Fix output and IP.
    todo: set IP in settings file instead.

--- dmail.rb
+++ dmail.rb
@@ -1,6 +1,7 @@
 require './environment'
+STDOUT.sync = true
 puts "initializing..."
-server = Server.new 2525, "127.0.0.1", 4
+server = Server.new 25, "130.237.234.173", 4
 puts "starting..."
 server.start
 puts "server started."
