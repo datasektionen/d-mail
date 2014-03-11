5dedc5915744e781a9db5291838b77b1cc882aef
Christian Avemark <christian@avemark.se>
2014-03-11 16:51:30 +0100

    Styyyle

--- lib/email_sender.rb
+++ lib/email_sender.rb
@@ -1,9 +1,11 @@
+# Sends emails
 class EmailSender
-
-  def initialize(redis = Redis.new)
-    @redis = redis
+  def initialize
+    @redis = Redis.new
     super
   end
+
+  attr_writer :redis
 
   def send(email_hash)
   end
