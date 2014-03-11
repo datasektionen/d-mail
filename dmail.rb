b2f46ce18fe0948ab4217e68421eba372d324fc0
Christian Avemark <christian@avemark.se>
2014-03-11 16:52:11 +0100

    Use config and introduce queue.

--- dmail.rb
+++ dmail.rb
@@ -3,9 +3,14 @@ STDOUT.sync = true
 puts "initializing..."
 
 if ARGV[0] == "production"
-  config = MiniConfig.load 'config/app.yml'
+  config = Miniconfig.load 'config/app.yml'
   server = Server.new config.server.port, config.server.ip, config.server.count
-  server.set_storage(ListStorage.new(Redis.new config.redis.path))
+  redis = Redis.new path: config.redis.path
+  redis.select config.list_index
+  server.set_storage(ListStorage.new(redis))
+  redis = Redis.new path: config.redis.path
+  redis.select config.queue_index
+  server.set_queue(MailQueue.new(redis))
 else
   server = Server.new 2525, "127.0.0.1", 4
 end
--- environment.rb
+++ environment.rb
@@ -3,6 +3,7 @@ require 'json'
 require 'mini-smtp-server'
 require 'logger'
 require 'miniconfig'
+require 'logger'
 
 require './lib/list_storage'
 require './lib/list_editor'
